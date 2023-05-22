2fcddc386d655a3cecd65ece0b0a6fac53ba69a5
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-05-22 13:54:01 +0300

    deal with static files

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
@@ -10,3 +12,8 @@ urlpatterns = [
         path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name = 'schema')),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
