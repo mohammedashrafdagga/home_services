53d84dc5cdda0ea29d62feadb7008c62991babf9
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-05-23 20:52:51 +0300

    editing code schema

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from .models import CodeActivate
+# from django.contrib import admin
+# from .models import CodeActivate
 
 
-# Register your models here.
-admin.site.register(CodeActivate)
+# # # Register your models here.
+# # admin.site.register(CodeActivate)
