571c4f275cc3d0ef770c53ea4b55c415de03b94f
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-05-25 13:25:02 +0300

    updating files

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,12 +1,8 @@
 
 import os
-from .settings import base
 
 from django.core.wsgi import get_wsgi_application
 
-if base.DEBUG:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.local")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
