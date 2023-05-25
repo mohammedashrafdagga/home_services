390e49c19631b729179add9520df81817f05529d
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-05-25 13:16:17 +0300

    editing asgi and wsgi files

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
+from settings import base
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+if base.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.local")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.production")
 
 application = get_wsgi_application()
