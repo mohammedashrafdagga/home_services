d3dd2bff5ec9166adf43a54dfc7500f478bed354
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-06-24 12:31:38 +0300

    editing setting

--- project/settings.py
+++ project/settings.py
@@ -100,13 +100,7 @@ DATABASES = {
        'USER': config('DATABASE_USER'),
        'PASSWORD': config('DATABASE_PASSWORD'),
        'HOST': config('DATABASE_HOST'),
-       'PORT': config('DATABASE_PORT'),
-       'OPTIONS': {
-            'options': '-c search_path=public',
-        },
-        'TEST': {
-            'CHARSET': 'utf8',
-        },
+       'PORT': config('DATABASE_PORT')
    }
 }
 
