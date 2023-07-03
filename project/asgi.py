574ed48c2097cf4c4b177c9d9e584deaa0678c95
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-07-03 09:30:21 +0300

    editing asgi and wsgi file

--- project/asgi.py
+++ project/asgi.py
@@ -1,8 +1,12 @@
-
 import os
-
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+# Set settings Module File
+if os.environ.get('DEBUG') == 'True':
+    settings_module = 'project.settings.local'
+else:
+    settings_module = 'project.settings.production'
+    
+    
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 application = get_asgi_application()
