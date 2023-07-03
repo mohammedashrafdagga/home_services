ccaf19f3cad9ea775699c9357a23945bf0e813b9
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-07-03 10:52:13 +0300

    refactoring static, media, templates file

--- project/settings.py
+++ project/settings.py
@@ -6,7 +6,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent / 'project'
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
